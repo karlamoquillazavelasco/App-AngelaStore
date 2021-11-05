e4923e2b303743a65110758d843c165a671292e6
karla moquillaza <karla_moquillaza@usmp.pe>
2021-11-05 01:29:47 -0500

    correcion bd borrada

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using App_AngelaStore.Data;
+
+[assembly: HostingStartup(typeof(App_AngelaStore.Areas.Identity.IdentityHostingStartup))]
+namespace App_AngelaStore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
